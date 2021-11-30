2578bafb714d341df4a9b00cecaa05fcbdd759f6
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-29 22:01:44 -0600

    Initial Commit

---
+++ final-project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  final-project
+//
+//  Created by Riley Johnston on 11/29/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

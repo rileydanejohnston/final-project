f954214e4f9d2c13189a294c5c4050ba28353773
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-06 17:36:06 -0600

    create basic framework in main, with guidelines for using the vector

--- final-project/main.cpp
+++ final-project/main.cpp
@@ -5,23 +5,84 @@
 #include "FarmAnimal.h"
 #include "Cow.h"
 #include "Chicken.h"
+#include <vector>
 #include <iostream>
 using namespace std;
 
 int main() {
     
-    Cow mooMoo(1.1, 2.2, "Cow", 5.5, 15, 2.5, 5.25);
-    Chicken cocky(1, 12, 200, "Chicken", 1.75, 6.3, 1.45, 3.67);
-    FarmAnimal *cowPtr = &mooMoo;
-    FarmAnimal *chickenPtr = &cocky;
     
-    cout << "Animal\t\tAge\t\tWeight\t\tGallons\t\tRevenue\t\t  Cost\t\tProfit" << endl;
-    cowPtr->display();
-    cowPtr->produce(10);
-    cowPtr->produce(10);
-    cowPtr->display();
-    cout << endl;
-    cout << "Animal\t\tAge\t\tWeight\t\t  Eggs\t\tUnits\t\tRevenue\t\t  Cost\t\tProfit" << endl;
-    chickenPtr->display();
+    
+    Cow mooMoo2(4.4, 3.3, "Cow", 2.64, 16, 4.4, 8.8);
+    Chicken cocky2(16, 12, 50, "Chicken", 1.1, 5.9, 2.87, 3.49);
+    
+    vector<FarmAnimal*> animals;
+    
+    char selection = 0;
+    
+    do {
+        // print menu
+        cout << "Farm Menu" << endl;
+        cout << "1) Add animal" << endl;
+        cout << "2) Delete animal" << endl;
+        cout << "3) Display animals" << endl;
+        cout << "4) Display farm" << endl;
+        cout << "5) Save farm to file" << endl;
+        cout << "q) Quit" << endl << endl;
+        cout << "Selection: ";
+        cin >> selection;
+        
+        if (selection == '1'){
+            
+            do {
+                cout << "1) Chicken" << endl;
+                cout << "2) Cow" << endl << endl;
+                cout << "Selection: ";
+                cin >> selection;
+                
+                if (selection == '1'){
+                    Chicken *cocky = new Chicken(1, 12, 200, "Chicken", 1.75, 6.3, 1.45, 3.67);
+                    animals.push_back(cocky);
+                    cout << "Added one chicken!" << endl;
+                }
+                else if (selection == '2'){
+                    Cow *mooMoo = new Cow(1.1, 2.2, "Cow", 5.5, 15, 2.5, 5.25);
+                    animals.push_back(mooMoo);
+                    cout << "Added one cow!" << endl;
+                }
+                else if (selection != '1' || selection != '2'){
+                    cout << "Invalid selection." << endl;
+                }
+            }
+            while (selection != '1' && selection != '2');
+            
+        }
+        else if(selection == '2'){
+        }
+        else if(selection == '3'){
+            // COWS cout << "Animal\t\tAge\t\tWeight\t\tGallons\t\tRevenue\t\t  Cost\t\tProfit" << endl;
+            // CHICKENS cout << "Animal\t\tAge\t\tWeight\t\t  Eggs\t\tUnits\t\tRevenue\t\t  Cost\t\tProfit" << endl;
+            cout << animals.size() << endl;
+            for (int i = 0; i < animals.size(); ++i){
+                animals[i]->display();
+            }
+        }
+        else if(selection == '4'){
+        }
+        else if(selection == 'q' || selection == 'Q'){
+            cout << "Thank you for using the program!" << endl;
+        }
+        else if(selection != 'q' || selection != 'Q'){
+            cout << "Invalid Selection." << endl;
+        }
+    }
+    while(selection != 'q' && selection != 'Q');
+    
+    
+    // free memory
+    for (int i = 0; i < animals.size(); ++i){
+        delete animals[i];
+    }
+    
     return 0;
 }
